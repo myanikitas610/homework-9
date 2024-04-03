f2032bb498aa3eedbd2b90020f5e98053950a2cf
Mya Nikitas <mnikitas@fordham.edu>
2024-04-03 17:52:19 -0400

    working on hw 9

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -41,11 +41,16 @@ class PostController
     }
 
     public function getPosts($id) {
+        $postModel = new Post(); 
         header("Content-Type: application/json");
         if ($id) {
             //TODO 5-c i: get a post data by id
+            $posts = $postModel->getPostById($id); 
         } else {
             //TODO 5-a: get all posts
+            $posts = $postModel->getAllPosts(); 
+            
+
         }
 
         exit();
@@ -59,6 +64,13 @@ class PostController
         $postData = $this->validatePost($inputData);
 
         //TODO 5-b: save a post
+        $post = new Post(); 
+        $post->savePost(
+            [
+                'title' => $postData['title'],
+                'description' => $postData['description'],
+            ]
+        );
 
         http_response_code(200);
         echo json_encode([
@@ -83,6 +95,15 @@ class PostController
         $postData = $this->validatePost($inputData);
 
         //TODO 5-c: update a post
+        $user = new Post();
+        $user->updatePost(
+            [
+                'id' => $id, 
+                'title' => $postData['title'],
+                'lastName' => $postData['description'],
+            ]
+        );
+
 
         http_response_code(200);
         echo json_encode([
@@ -98,6 +119,12 @@ class PostController
         }
 
         //TODO 5-d: delete a post
+        $post = new Post();
+        $post->deletePost(
+            [
+                'id' => $id,
+            ]
+        );
 
         http_response_code(200);
         echo json_encode([
--- sql/question-3-answers.sql
+++ sql/question-3-answers.sql
@@ -1,16 +1,32 @@
 -- select by id 1:
-
+Select *
+From posts
+Where id = 1;
 
 -- select all posts where title = includes "title 2":
-
+Select *
+From posts
+Where title Like '%title 2%';
 
 -- select all posts and order by the title column alphabetically:
+Select *
+From posts
+Order By title;
 
 
 -- insert 3 new posts
+Insert into posts (title, description) values
+('New Post 1', 'Description for New Post 1'),
+('New Post 2', 'Description for New Post 2'),
+('New Post 3', 'Description for New Post 3');
 
 
 -- update posts where id = 1, set the title to "new title"
+Update posts 
+Set title = 'new title'
+Where id = 1;
 
 
 -- delete post where id = 2
+Delete From posts
+Where id = 2;
